6d4742e43031f41e7d4300713c7bf103f4d18dfd
carlogilmar <carlogilmar12@gmail.com>
2019-02-24 18:15:24 -0600

    Validate if the device is able to test the app

--- assets/js/rock_controller.js
+++ assets/js/rock_controller.js
@@ -11,9 +11,9 @@ export const app = new Vue({
 		beta: "",
 		gama: "",
 		alpha: "",
-		x: "",
-		y: "",
-		z: "",
+		x: null,
+		y: null,
+		z: null,
 		abeta: "",
 		agama: "",
 		aalpha: "",
@@ -30,30 +30,18 @@ export const app = new Vue({
         this.message = " 😊 ";
       })
       .receive("error", resp => { console.log("Unable to join", resp) });
-    window.addEventListener('deviceorientation', this.orientation);
     window.addEventListener('devicemotion', this.motion);
   },
   methods:{
-    orientation: function(e){
-   		this.beta = Math.round(e.beta);
-   		this.gama = Math.round(e.gamma);
-   		this.alpha = Math.round(e.alpha);
-    },
     motion: function(e){
-   		//this.abeta = Math.round( e.rotationRate.beta );
-   		//this.agama = Math.round( e.rotationRate.gamma );
-   		//this.aalpha = Math.round( e.rotationRate.alpha );
 			this.x = Math.round(e.acceleration.x *10)/10;
 			this.y = Math.round(e.acceleration.y *10)/10;
 			this.z = Math.round(e.acceleration.z *10)/10;
-			this.agx = Math.round(e.accelerationIncludingGravity.x *10)/10;
-			this.agy = Math.round(e.accelerationIncludingGravity.y *10)/10;
-			this.agz = Math.round(e.accelerationIncludingGravity.z *10)/10;
       this.movement();
 		},
     movement: function(){
       if( this.x < 0.5 && this.y < 0.5 && this.z < 0.5 ){
-        this.mvn = "";
+        this.mvn = "-";
       } else if (this.x > 15){
         this.mvn = "Lateral";
       } else if (this.y > 15){
