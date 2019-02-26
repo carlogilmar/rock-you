f5d261e830e8da572e97d161021891fe1371027d
carlogilmar <carlogilmar12@gmail.com>
2019-02-25 21:42:18 -0600

    Adding orientartion function

--- assets/js/playme_controller.js
+++ assets/js/playme_controller.js
@@ -42,30 +42,44 @@ export const app = new Vue({
 			this.x = Math.round(e.acceleration.x *10)/10;
 			this.y = Math.round(e.acceleration.y *10)/10;
 			this.z = Math.round(e.acceleration.z *10)/10;
-      this.movement();
 		},
     orientation_check:function(){
       if( this.x && this.y && this.z && this.beta && this.gama ){
         if( this.beta > 1 && this.beta <15 && this.gama < 5){
           this.orn = "Inclinado";
+          this.sound_piano.volume(0);
         } else if( this.beta > 50 && this.beta < 100 && this.gama < 5 ){
           this.orn = "Parado";
+          this.sound_piano.volume(0);
         } else if( this.gama > 35 && this.gama < 80){
           this.orn = "De costado";
+          this.movement_3();
         }
+      }
+    },
+    movement_3: function(){
+      if( this.x < 0.5 && this.y < 0.5 && this.z < 0.5 ){
+        this.mvn = "";
+        this.sound_piano.volume(0);
+      } else if (this.x > 3 || this.y > 3 || this.z > 3 ){
+        this.mvn = "Lateral";
+        this.sound_piano.volume(1);
+      } else {
+        this.mvn = "";
+        this.sound_piano.volume(0);
       }
     },
     movement: function(){
       if( this.x < 0.5 && this.y < 0.5 && this.z < 0.5 ){
         this.mvn = "-";
         this.sound_piano.volume(0);
-      } else if (this.x > 1.5){
+      } else if (this.x > 3){
         this.mvn = "Lateral";
         this.sound_piano.volume(1);
-      } else if (this.y > 1.5){
+      } else if (this.y > 3){
         this.mvn = "Frontal";
         this.sound_piano.volume(1);
-      } else if (this.z > 1.5){
+      } else if (this.z > 3){
         this.mvn = "Descendente";
         this.sound_piano.volume(1);
       } else{
